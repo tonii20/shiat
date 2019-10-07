5b4c5595da946c2d9b072e9981a07c912a3a35ca
toni20 <antonius.peer@tum.de>
2019-10-07 21:42:33 +0200

    Ball

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -18,6 +21,7 @@ import androidx.navigation.ui.NavigationUI;
 
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
@@ -36,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Image
 
-    private ImageView player1, player2, fussball;
+    ImageView player1, player2, fussball;
 
 
     //Size
@@ -47,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
 
     private float player1X, player1Y;
     private float player2X, player2Y;
-    private float ballX, ballY;
 
     //Score
 
@@ -55,61 +58,37 @@ public class MainActivity extends AppCompatActivity {
 
     private int score1, score2;
     //Status
-
     private boolean start_flg = false;
     TextView pitchText;
     TextView noteText;
+
+    //Handler
+    Timer timer;
+    Handler handler;
+
+    //Ball
+    public int[] direction = new int[]{1,1}; //direction modifier (-1,1)
+    public int speed = 13;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AudioDispatcher dispatcher =
-                AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
-
-
-        pitchText = (TextView) findViewById(R.id.scorePlayer1);
-      noteText = (TextView) findViewById(R.id.scorePlayer2);
-
-        PitchDetectionHandler pdh = new PitchDetectionHandler() {
-            @Override
-            public void handlePitch(PitchDetectionResult res, AudioEvent e) {
-                final float pitchInHz = res.getPitch();
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        processPitch(pitchInHz);
-
-
-                    }
-                });
-            }
-
-
-        };
-        // pitchText.setText("geht  dsafsdf");
-
-        AudioProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
-        dispatcher.addAudioProcessor(pitchProcessor);
-
-
-        Thread audioThread = new Thread(dispatcher, "Audio Thread");
-        audioThread.start();
-
-
         gameFrame = findViewById(R.id.gameFrame);
         startLayout = findViewById(R.id.startLayout);
         player1 = findViewById(R.id.player1);
         player2 = findViewById(R.id.player2);
-       // fussball = findViewById(R.id.fussball);
+        fussball = findViewById(R.id.fussball);
         scorePlayer1 = findViewById(R.id.scorePlayer1);
         scorePlayer2 = findViewById(R.id.scorePlayer2);
+
     }
 
     public void processPitch(float pitchInHz) {
 
-        pitchText.setText("geht " + pitchInHz);
         float laenge = frameWidth/7;
 
         //  if(pitchInHz >= 80 && pitchInHz < 220) {
@@ -132,29 +111,85 @@ public class MainActivity extends AppCompatActivity {
             player1X = 6 *laenge;
         }
 
-        if(start_flg) {
+
             player1.setX(player1X);
             player2.setX(player2X);
-            pitchText.setText("geht1333     " + pitchInHz);
-        }
 
-        //   }else{
-        //     action_flg = false;
-        //   }
+
+
+    }
+    private class Task1 implements Runnable {
+        @Override
+        public void run() {
+            pitchText.setText("gsdafdsafdsa");
+            move();
+            handler.postDelayed(this, 100);
+        }
     }
 
-
     public void startGame(View view) {
-        start_flg = true;
+
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
+
+
+        pitchText = (TextView) findViewById(R.id.scorePlayer1);
+        noteText = (TextView) findViewById(R.id.scorePlayer2);
+
+
+
+        handler  = new Handler();
+       Task1 task1 = new Task1();
+        handler.postDelayed(task1, 100);
+
+
+        /*Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+
+                        scorePlayer1.setText("jjjjjj");
+                        //move();
+
+            }
+
+        }, 0, 100);
+*/
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+
+            @Override
+            public void handlePitch(PitchDetectionResult res, AudioEvent e) {
+                final float pitchInHz = res.getPitch();
+                runOnUiThread(
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                processPitch(pitchInHz);
+                                move();
+
+
+                            }
+                        });
+            }
+
+        };
+
+        AudioProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(pitchProcessor);
+
+
+        Thread audioThread = new Thread(dispatcher, "Audio Thread");
+        audioThread.start();
+
         startLayout.setVisibility(View.INVISIBLE);
 
-        if ( frameHeight == 0 ) {
+        if (frameHeight == 0) {
             frameHeight = gameFrame.getHeight();
             frameWidth = gameFrame.getWidth();
             initialFrameWidth = frameWidth;
 
-            player1Size = frameWidth/7;
-            player2Size = frameWidth/7;
+            player1Size = frameWidth / 7;
+            player2Size = frameWidth / 7;
 
             player1X = player1.getX();
             player1Y = player1.getY();
@@ -166,10 +201,10 @@ public class MainActivity extends AppCompatActivity {
         player1.setX(375.0f);
         player2.setX(375.0f);
 
-        //fussball.setX(500.0f);
-       // fussball.setY(670.0f);
+        fussball.setX(500.0f);
+        fussball.setY(670.0f);
 
-        //fussball.setVisibility(View.VISIBLE);
+        fussball.setVisibility(View.VISIBLE);
         player1.setVisibility(View.VISIBLE);
         player2.setVisibility(View.VISIBLE);
 
@@ -180,23 +215,37 @@ public class MainActivity extends AppCompatActivity {
         scorePlayer2.setText("Score: 0");
 
 
-        /*timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                if(start_flg) {
-                    handler.post(new Runnable(){
-                        @Override
-                        public void run() {
-                            changePos();
-                        }
-                    });
-                }
-            }
-        },0,20);*/
-
 
     }
+
+
+    public RectF oval;
+        public void move() {
+
+            scorePlayer2.setText("nois");
+            fussball.setX(fussball.getX() + speed * direction[0]);
+            fussball.setY(fussball.getY() + speed * direction[0]);
+            int size = fussball.getWidth();
+            this.oval = new RectF(fussball.getX() - size / 2,
+                    fussball.getY() - size / 2, fussball.getX() + size / 2,
+                    fussball.getY() + size / 2);
+            Rect bounds = new Rect();
+            this.oval.roundOut(bounds);
+
+
+            //This is what you're looking for ▼
+
+            if (!gameFrame.getClipBounds(bounds)) {
+                if (fussball.getX() - size < 0 || fussball.getX() + size > gameFrame.getWidth()) {
+                    direction[0] = direction[0] * -1;
+                }
+                if (fussball.getY() - size < 0 || fussball.getY() + size > gameFrame.getHeight()) {
+                    direction[1] = direction[1] * -1;
+                }
+            }
+        }
+
+
     public void quitGame(View view) {
 
     }
